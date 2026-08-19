a200e84813b884130a45342d138fcadf172d77f7
zaydthatswho <149309941+zaydthatswho@users.noreply.github.com>
2026-08-19 11:25:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,31 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
+import datetime
 import smtplib
-import os
+from email.message import EmailMessage
+
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+def send_email(day):
+    msg = EmailMessage()
+    msg["Subject"] = "It's Raise Day B*tches!"
+    msg["From"] = USERNAME
+    msg["To"] = USERNAME
+    msg.set_content(f"It's the {day}th today! Gather your balls and ask for a raise!")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(USERNAME, PASSWORD)
+        connection.send_message(msg)
+
+
+
+x = datetime.datetime.now()
+day_of_month = x.strftime("%d")
+
+if int(day_of_month) == 19:
+    send_email(day_of_month)
